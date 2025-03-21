cbf006b3f69c3c2bdd0145c19b9d94c8684f919e
saipavangs <saipavangangishetti@gmail.com>
2025-03-20 22:55:49 -0400

    Remove unwanted folders and files

--- django-bootstrap-basicproject/frontendBootstrap/index.js
+++
@@ -1,9 +0,0 @@
-console.log("welcome to java script");
-// let a=18;
-// //let ,var,const
-// if (a<15){
-//   console.log("TRUE")
-// }
-// else{
-//   console.log("FALSE")
-// }
--- dsa/namefunderstand.py
+++
@@ -1,2 +0,0 @@
-import mainfunderstand
-print("hi")
